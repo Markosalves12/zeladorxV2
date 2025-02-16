e1faa851c544058f896792c2aeb2e1bff4593611
Markos Alves pereira <101573252+Markosalves12@users.noreply.github.com>
2025-02-16 18:01:57 -0300

    dimensionador

--- medidor/urls.py
+++ medidor/urls.py
@@ -3,6 +3,6 @@ from medidor.views import medidor_upload
 from medidor.utils import processar_poligonos
 
 urlpatterns = [
-    path('medidor/<str:userid>/', medidor_upload, name='medidor_upload'),
+    path('Dimensionador/<str:userid>/', medidor_upload, name='medidor_upload'),
     path('processar_poligonos/<str:id_random>/', processar_poligonos, name='processar_poligonos'),
 ]
